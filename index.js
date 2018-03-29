975f111bfd7c438a37064a28cb3ce5f4ea61b6b9
timster788 <timster78811@gmail.com>
2018-03-29 20:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ function theBeatlesPlay(musicians, instruments){
    }
    return newArr
 }
- function johnLennonFacts(){
-   var facts = ["He was the last Beatle to learn to drive" ,
-   "He was never a vegetarian" ,
-   "He was a choir boy and boy scout" ,
-   "He hated the sound of his own voice"];
+var facts = ["He was the last Beatle to learn to drive" ,
+"He was never a vegetarian" ,
+"He was a choir boy and boy scout" ,
+"He hated the sound of his own voice"];
+ function johnLennonFacts(facts){
+
      var newFacts = [];
      var i = 0
      while (i < facts.length) {
